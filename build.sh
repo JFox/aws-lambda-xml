6c8d3381f3659a7fb91c16ee8382561835d93ef8
Al Johri <al.johri@gmail.com>
2018-09-01 17:23:49 -0400

    add 4.2.4

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+version=4.2.4
+
+mkdir -p "$version/py{27|36}"
+
+docker run -v $(pwd)/"$version/py27/":/outputs -it lambci/lambda:build-python2.7 pip install lxml=="$version" -t /outputs/
+docker run -v $(pwd)/"$version/py36/":/outputs -it lambci/lambda:build-python3.6 pip install lxml=="$version" -t /outputs/
