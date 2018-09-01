f324fbd7bd2ee60f62f51b2d7c063a8d5bdaf504
Al Johri <al.johri@gmail.com>
2018-09-01 17:33:21 -0400

    add tgz files

--- build.sh
+++ build.sh
@@ -7,3 +7,6 @@ mkdir -p "$version"/py{27,36}
 
 docker run -v $(pwd)/"$version/py27/":/outputs -it lambci/lambda:build-python2.7 pip install lxml=="$version" -t /outputs/
 docker run -v $(pwd)/"$version/py36/":/outputs -it lambci/lambda:build-python3.6 pip install lxml=="$version" -t /outputs/
+
+tar -czvf "$version/py36/lxml-4.2.4.tgz" "$version/py36/lxml"
+tar -czvf "$version/py27/lxml-4.2.4.tgz" "$version/py27/lxml"
