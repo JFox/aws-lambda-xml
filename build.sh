4e8c98f5763c01a5f9950c0e67534b99c58b9f9f
Al Johri <al.johri@gmail.com>
2018-09-01 17:26:59 -0400

    update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 version=4.2.4
 
-mkdir -p "$version/py{27|36}"
+rm -rf "$version"
+mkdir -p "$version"/py{27,36}
 
 docker run -v $(pwd)/"$version/py27/":/outputs -it lambci/lambda:build-python2.7 pip install lxml=="$version" -t /outputs/
 docker run -v $(pwd)/"$version/py36/":/outputs -it lambci/lambda:build-python3.6 pip install lxml=="$version" -t /outputs/
